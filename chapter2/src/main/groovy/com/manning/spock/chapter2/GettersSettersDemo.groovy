3d602eb84c0e212830ea2bfcae07e0b479105f9b
Kostis Kapelonis <kkapelon@gmail.com>
2014-12-21 19:33:51 +0200

    Example for Getters and Setters in Groovy

---
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/GettersSettersDemo.groovy
@@ -0,0 +1,14 @@
+package com.manning.spock.chapter2
+
+class GettersSettersDemo {
+	
+	public static void main(String[] args)
+	{
+		Person person = new Person()
+		person.setFirstName("Lyta")
+		person.setLastName("Alexander")
+		
+		System.out.println("Person first name is "+person.getFirstName())
+		System.out.println("Person name name is "+person.getLastName())
+	}
+}
---
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/Person.groovy
@@ -0,0 +1,13 @@
+package com.manning.spock.chapter2
+
+class Person {
+	String firstName
+	String lastName
+	int age
+	String role
+
+	
+}
+
+
+
