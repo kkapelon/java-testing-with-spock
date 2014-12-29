0ca96447dcafacd44335a615594616b426498cfa
Kostis Kapelonis <kkapelon@gmail.com>
2014-12-29 20:26:12 +0200

    Creating Maps in Groovy

---
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/GroovyCollections.groovy
@@ -0,0 +1,29 @@
+package com.manning.spock.chapter2
+
+//Creating a map in Java
+
+
+Map<String,Integer> wordCounts = new HashMap<>();
+wordCounts.put("Hello",1);
+wordCounts.put("Java",1);
+wordCounts.put("World",2);
+
+//Creating a map in Groovy
+
+Map<String,Integer> wordCounts2 = ["Hello":1,"Groovy":1,"World":2]
+
+
+//Creating a map with objects
+Employee person1 = new Employee(firstNamse:"Alice",lastName:"Olson",age:30)
+Employee person2 = new Employee(firstName:"Jones",lastName:"Corwin",age:45)
+
+Address address1 = new Address(street:"Marley",number:25)
+Address address2 = new Address(street:"Barnam",number:7)
+
+Map<Employee,Address> staffAddresses = new HashMap<>();
+staffAddresses.put(person1, address1);
+staffAddresses.put(person2, address2);
+
+Map<Employee,Address> staffAddresses2 = [(person1):address1,(person2):address2]
+
+assert staffAddresses[person2].street == "Barnam"
---
+++ chapter2/src/main/java/com/manning/spock/chapter2/Address.java
@@ -0,0 +1,34 @@
+package com.manning.spock.chapter2;
+
+public class Address {
+	private String country;
+	private String street;
+	private String postCode;
+	private int number;
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	
+}
