aa8057443cd004d777a5aa733c2e004ba1a5739f
Kostis Kapelonis <kkapelon@gmail.com>
2014-12-30 15:16:44 +0200

    Examples with Groovy lists and maps

--- chapter2/src/main/groovy/com/manning/spock/chapter2/GroovyCollections.groovy
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/GroovyCollections.groovy
@@ -40,3 +40,33 @@ String[] racesArray = ["Drazi", "Minbari", "Humans"]
 assert racesArray[0] == "Drazi"
 assert races[0] == "Drazi"
 assert racesArray.size() == races2.size()
+
+//Accessing lists
+
+List<String> humanShips = ["Condor","Explorer"]
+assert humanShips.get(0) == "Condor"
+assert humanShips[0] == "Condor"
+humanShips.add("Hyperion")
+humanShips << "Nova" << "Olympus"
+assert humanShips[3] == "Nova"
+assert humanShips[4] == "Olympus"
+humanShips[3] = "Omega"
+assert humanShips[3] == "Omega"
+
+//Using an non-existing index
+humanShips[8] = "Warlock"
+assert humanShips[8] == "Warlock"
+assert humanShips[7] == null
+
+
+//Accessing maps
+Map<String,String> personRoles = [:]
+personRoles.put("Suzan Ivanova","Lt. Commander")
+personRoles["Stephen Franklin"]= "Doctor"
+
+assert personRoles.get("Suzan Ivanova") == "Lt. Commander"
+assert personRoles["Stephen Franklin"] == "Doctor"
+
+personRoles["Suzan Ivanova"]= "Commander"
+assert personRoles["Suzan Ivanova"] == "Commander"
+
