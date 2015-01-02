0c22b3923219fa6b4f03a5746a9467b39c1ee218
Kostis Kapelonis <kkapelon@gmail.com>
2015-01-02 14:41:44 +0200

    Example of Groovy "with"

--- chapter2/src/main/groovy/com/manning/spock/chapter2/GettersSettersDemo3.groovy
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/GettersSettersDemo3.groovy
@@ -8,10 +8,18 @@ class Person2 {
 }
 
 Person2 person = new Person2()
-person.firstName = "Susan "
+person.firstName = "Susan"
 person.lastName = "Ivanova"
-person.rank = "Commander "
+person.rank = "Commander"
 
 println "Person first name is $person.firstName"
 println "Person last name is $person.lastName"
 println "Person rank is $person.rank"
+
+
+person.with 
+{
+	assert firstName == "Susan"
+	assert lastName == "Ivanova"
+	assert rank == "Commander"
+}
