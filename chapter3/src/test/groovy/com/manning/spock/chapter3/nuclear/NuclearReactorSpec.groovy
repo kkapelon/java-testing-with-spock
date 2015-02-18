1e56c38bb8b434ec12b6511a253df405bc785556
Jonatas Emidio <jonatasemidio@gmail.com>
2015-02-18 14:30:11 -0200

    Just for readability
    Can we change just for readability?

--- chapter3/src/test/groovy/com/manning/spock/chapter3/nuclear/NuclearReactorSpec.groovy
+++ chapter3/src/test/groovy/com/manning/spock/chapter3/nuclear/NuclearReactorSpec.groovy
@@ -20,19 +20,19 @@ class NuclearReactorSpec extends spock.lang.Specification{
 		status.evacuationMinutes == evacuation
 		
 		where: "possible nuclear incidents are:"
-		pressure | fireSensors | radiation || alarm | shutDown | evacuation
-		150      | 0           | []        || false | false    | -1
-		150      | 1           | []        || true  | false    | -1
-		150      | 3           | []        || true  | true     | -1
-		150      | 0| [110.4f ,0.3f, 0.0f] || true  | true     | 1
-		150      | 0| [45.3f ,10.3f, 47.7f]|| false | false    | -1
-		155      | 0| [0.0f ,0.0f, 0.0f]   || true  | false    | -1
-		170      | 0| [0.0f ,0.0f, 0.0f]   || true  | true     | 3
-		180      | 0| [110.4f ,0.3f, 0.0f] || true  | true     | 1
-		500      | 0| [110.4f ,300f, 0.0f] || true  | true     | 1
-		30       | 0|[110.4f ,1000f, 0.0f] || true  | true     | 1
-		155      | 4| [0.0f ,0.0f, 0.0f]   || true  | true     | -1
-		170      | 1| [45.3f ,10.3f, 47.7f]|| true  | true     | 3
+		pressure | fireSensors | radiation             || alarm | shutDown | evacuation
+		150      | 0           | []                    || false | false    | -1
+		150      | 1           | []                    || true  | false    | -1
+		150      | 3           | []                    || true  | true     | -1
+		150      | 0           | [110.4f ,0.3f, 0.0f]  || true  | true     | 1
+		150      | 0           | [45.3f ,10.3f, 47.7f] || false | false    | -1
+		155      | 0           | [0.0f ,0.0f, 0.0f]    || true  | false    | -1
+		170      | 0           | [0.0f ,0.0f, 0.0f]    || true  | true     | 3
+		180      | 0           | [110.4f ,0.3f, 0.0f]  || true  | true     | 1
+		500      | 0           | [110.4f ,300f, 0.0f]  || true  | true     | 1
+		30       | 0           | [110.4f ,1000f, 0.0f] || true  | true     | 1
+		155      | 4           | [0.0f ,0.0f, 0.0f]    || true  | true     | -1
+		170      | 1           | [45.3f ,10.3f, 47.7f] || true  | true     | 3
 	}
 	
 }
