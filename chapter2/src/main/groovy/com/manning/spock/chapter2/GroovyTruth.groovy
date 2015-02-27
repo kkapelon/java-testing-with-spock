3dfae41781310656078d111bf167e09107dc120c
Kostis Kapelonis <kkapelon@gmail.com>
2015-02-27 22:26:47 +0200

    Added more examples with regular expressions for Groovy truth.

--- chapter2/src/main/groovy/com/manning/spock/chapter2/GroovyTruth.groovy
+++ chapter2/src/main/groovy/com/manning/spock/chapter2/GroovyTruth.groovy
@@ -41,11 +41,24 @@ Pattern myRegex = ~/needle/
 assert myRegex.matcher("needle in haystack")
 assert !myRegex.matcher("Wrong haystack")
 
+//Regular expression shortcut with the =~ operator
+assert "needle in haystack" =~/needle/
+assert !("Wrong haystack" =~/needle/)
+
+//All closures are assumed to be "true"
 def closure = { number -> number+2 }
 assert closure
 
 println "Script has finished because all asserts pass"
 
+/*
+ * Fun with Groovy truth 
+ * 
+ * This is valid Groovy code: boolean flag = -45. 
+ * Even though this line does not even compile in Java, 
+ * in Groovy the number -45 is a non-zero number and therefore 
+ * the variable flag is now true. Isn't that fun?
+ */
 boolean flag = -42
 assert flag
 
