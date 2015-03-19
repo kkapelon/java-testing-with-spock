83339de50f1fabbf95b1850e99ab727015936b24
Kostis Kapelonis <kkapelon@gmail.com>
2015-03-20 00:03:10 +0200

    Added examples of descriptive tests.

---
+++ chapter4/src/test/groovy/com/manning/spock/chapter4/specification/BasketWeightSpec.groovy
@@ -0,0 +1,48 @@
+package com.manning.spock.chapter4.specification
+
+import spock.lang.*
+
+import com.manning.spock.chapter4.Basket
+import com.manning.spock.chapter4.Product
+
+@Narrative("""" A empty basket start with no weight. Adding products to the basket
+increases its weight. The weight is then used for billing during shipping calculations.
+Electronic goods have always 0 zero weight.
+""")
+@Title("Unit test for basket weight")
+class BasketWeightSpec extends spock.lang.Specification{
+
+	def "A basket with two products weights as their sum"() {
+		given: "an empty basket"
+		Basket basket = new Basket()
+
+		and: "several products"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+
+		when: "user wants to buy the TV and the camera and the hifi"
+		basket.addProduct tv
+		basket.addProduct camera
+
+		then: "basket weight is equal to all product weight"
+		basket.currentWeight == (tv.weight + camera.weight)
+	}
+
+	def "A basket with two products weights as their sum (better)"() {
+		given: "an empty basket"
+		@Subject
+		Basket basket = new Basket()
+
+		and: "several products"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+
+		when: "user wants to buy the TV and the camera and the hifi"
+		basket.addProduct tv
+		basket.addProduct camera
+
+		then: "basket weight is equal to all product weight"
+		basket.currentWeight == (tv.weight + camera.weight)
+	}
+}
+
