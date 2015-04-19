dd981a5e7f51732432b15865c45ae3506877fc34
Kostis Kapelonis <kkapelon@gmail.com>
2015-04-19 22:32:28 +0300

    Simplified data table listing

--- chapter5/src/test/groovy/com/manning/spock/chapter5/discount/DiscountSpec.groovy
+++ chapter5/src/test/groovy/com/manning/spock/chapter5/discount/DiscountSpec.groovy
@@ -16,13 +16,13 @@ class DiscountSpec extends spock.lang.Specification{
 		and: "several conditions that affect them"
 		product.with {
 			price = price
-			dailyDeal = specialDeal
+			dailyDeal = special
 			discountPercent = discount
 		}
 		customer.with
 		{
-			vip = vipStatus
-			bonusPoints = currentPoints
+			vip = isVip
+			bonusPoints = points
 		}
 		currentOrder.totalPrice = order
 		
@@ -30,8 +30,19 @@ class DiscountSpec extends spock.lang.Specification{
 		checkout.calculateDiscountPercentFor(product, currentOrder) == finalDiscount
 
 		where: "some of the possible scenarios are"
-		price | vipStatus | currentPoints | order | discount | specialDeal  || finalDiscount
-		50    | false     | 0             | 0     |      0   |   false      || 0
+		price | isVip | points | order | discount | special  || finalDiscount
+		50    | false | 0      | 50    |      0   |  false   || 0
+		100   | false | 0      | 300   |      0   |  false   || 10
+		500   | false | 0      | 0     |      0   |  true    || 50
+		50    | true  | 0      | 50    |      0   |  false   || 15
+		50    | true  | 0      | 50    |      25  |  false   || 25
+		50    | true  | 0      | 50    |      5   |  false   || 15
+		50    | true  | 0      | 50    |      5   |  true    || 50
+		50    | false | 0      | 100   |      0   |  false   || 0
+		50    | false | 0      | 75    |      10  |  false   || 10
+		50    | false | 5000   | 50    |      0   |  false   || 75
+		50    | false | 3000   | 50    |      0   |  false   || 0
+		50    | true  | 8000   | 50    |      3   |  false   || 75
 	}
 }
 
