36610163480f90b72d18a3b962b56bbe0779e8f9
Kostis Kapelonis <kkapelon@gmail.com>
2015-04-19 22:52:01 +0300

    Fixed lifecyle test with data tables.

--- chapter5/src/test/groovy/com/manning/spock/chapter5/tables/LifecycleDataSpec.groovy
+++ chapter5/src/test/groovy/com/manning/spock/chapter5/tables/LifecycleDataSpec.groovy
@@ -6,81 +6,36 @@ import spock.lang.*
 
 class LifecycleDataSpec extends spock.lang.Specification{
 
+	def setup() {
+		println "Setup prepares next run"
+	}
+	
 	def "Trivial adder test"() {
 		given: "an adder"
 		Adder adder = new Adder()
-//		println "Given: block runs"
+		println "Given: block runs"
 		
 		when:"the add method is called for two numbers"
 		int result = adder.add(first,second)
-//		println "When: block runs for $first and $second"
+		println "When: block runs for first = $first and second = $second"
 
 		then: "the result should be the sum of them"
 		result ==sum
-//		println "Then: block is evaluated for $sum"
+		println "Then: block is evaluated for sum = $sum"
 
 		where: "some scenarios are"
-		first   |second     || sum
-		1       | 1         || 2
-		3       | 2         || 5
-		3       | -3        || 0
+		first   |second || sum
+		1       | 1     || 2
+		3       | 2     || 5
+		3       | -3    || 0
 	}
 	
-	@Unroll
-	def "Trivial adder test (alt)"() {
-		given: "an adder"
-		Adder adder = new Adder()
-		
-		when:"the add method is called for two numbers"
-		int result = adder.add(first,second)
-
-		then: "the result should be the sum of them"
-		result ==sum
-
-		where: "some scenarios are"
-		first   |second     || sum
-		1       | 1         || 2
-		3       | 2         || 5
-		3       | -3        || 0
+	def cleanup()
+	{
+		println "Cleanup releases resources of last run\n"
 	}
 	
 	
-	
-	@Unroll("Adder test #first, #second, #sum (alt2)")
-	def "Trivial adder test (alt3)"() {
-		given: "an adder"
-		Adder adder = new Adder()
-		
-		when:"the add method is called for two numbers"
-		int result = adder.add(first,second)
-
-		then: "the result should be the sum of them"
-		result ==sum
-
-		where: "some scenarios are"
-		first   |second     || sum
-		1       | 1         || 2
-		3       | 2         || 5
-		3       | -3        || 0
-	}
-	
-	@Unroll
-	def "Adder test #first, #second, #sum (alt3)"() {
-		given: "an adder"
-		Adder adder = new Adder()
-		
-		when:"the add method is called for two numbers"
-		int result = adder.add(first,second)
-
-		then: "the result should be the sum of them"
-		result ==sum
-
-		where: "some scenarios are"
-		first   |second     || sum
-		1       | 1         || 2
-		3       | 2         || 5
-		3       | -3        || 0
-	}
 	
 }
 
