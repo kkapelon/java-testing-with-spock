54bd08a5cb553ed17fa4a5cb76046b511da809d0
Kostis Kapelonis <kkapelon@gmail.com>
2015-05-21 00:12:44 +0300

    Example with custom helper method for mocks

---
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/HelperMockingMethodSpec.groovy
@@ -0,0 +1,43 @@
+package com.manning.spock.chapter6.mocks
+
+import spock.lang.*
+
+import com.manning.spock.chapter6.Basket
+import com.manning.spock.chapter6.Product
+import com.manning.spock.chapter6.stubs.ShippingCalculator;
+import com.manning.spock.chapter6.stubs.WarehouseInventory
+
+@Subject(Basket.class)
+class HelperMockingMethodSpec extends spock.lang.Specification{
+
+	def "Only warehouse is queried when checking shipping status"() {
+		given: "an basket, a TV and a camera"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		Basket basket = new Basket()
+
+		and:"a warehouse with limitless stock"
+		WarehouseInventory inventory = Mock(WarehouseInventory)
+		basket.setWarehouseInventory(inventory)
+		ShippingCalculator shippingCalculator = Mock(ShippingCalculator)
+		basket.setShippingCalculator(shippingCalculator)
+
+		when: "user checks out both products"
+		basket.addProduct tv
+		basket.addProduct camera
+		boolean readyToShip = basket.canShipCompletely()
+
+		then: "order can be shipped"
+		readyToShip
+		interaction { 
+			myHelperMethod(inventory,basket ) 
+		}
+	}
+
+	def myHelperMethod(WarehouseInventory inventory, Basket basket) {
+		basket.getProductTypesCount() == 2
+		2 * inventory.availableOfProduct( _ , _) >> true
+		0 * _
+	}
+}
+
