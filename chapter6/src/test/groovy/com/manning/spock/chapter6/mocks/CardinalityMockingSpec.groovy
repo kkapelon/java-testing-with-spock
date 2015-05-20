446309eb96b6224c6eab49d81f800367081403cd
Kostis Kapelonis <kkapelon@gmail.com>
2015-05-20 23:41:13 +0300

    Added cardinality test.

---
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/CardinalityMockingSpec.groovy
@@ -0,0 +1,84 @@
+package com.manning.spock.chapter6.mocks
+
+import spock.lang.*
+
+import com.manning.spock.chapter6.Basket
+import com.manning.spock.chapter6.Product
+import com.manning.spock.chapter6.stubs.ShippingCalculator;
+import com.manning.spock.chapter6.stubs.WarehouseInventory
+
+@Subject(Basket.class)
+class CardinalityMockingSpec extends spock.lang.Specification{
+
+	def "Warehouse is queried for each product"() {
+		given: "an basket, a TV and a camera"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		Basket basket = new Basket()
+		
+		and:"a warehouse with limitless stock"
+		WarehouseInventory inventory = Mock(WarehouseInventory)
+		basket.setWarehouseInventory(inventory)
+
+		when: "user checks out both products"
+		basket.addProduct tv
+		basket.addProduct camera
+		boolean readyToShip = basket.canShipCompletely()
+
+		then: "order can be shipped"
+		readyToShip
+		2 * inventory.availableOfProduct( _ , _) >> true
+		0 * inventory.preload(_ , _)
+	}
+	
+	def "Warehouse is queried for each product - strict"() {
+		given: "an basket, a TV and a camera"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		Basket basket = new Basket()
+		
+		and:"a warehouse with limitless stock"
+		WarehouseInventory inventory = Mock(WarehouseInventory)
+		basket.setWarehouseInventory(inventory)
+
+		when: "user checks out both products"
+		basket.addProduct tv
+		basket.addProduct camera
+		boolean readyToShip = basket.canShipCompletely()
+
+		then: "order can be shipped"
+		readyToShip
+		2 * inventory.availableOfProduct( _ , _) >> true
+		0 * inventory._
+	}
+	
+	def "Only warehouse is queried when checking shipping status"() {
+		given: "an basket, a TV and a camera"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		Basket basket = new Basket()
+		
+		and:"a warehouse with limitless stock"
+		WarehouseInventory inventory = Mock(WarehouseInventory)
+		basket.setWarehouseInventory(inventory)
+		ShippingCalculator shippingCalculator = Mock(ShippingCalculator)
+		basket.setShippingCalculator(shippingCalculator)
+
+		when: "user checks out both products"
+		basket.addProduct tv
+		basket.addProduct camera
+		boolean readyToShip = basket.canShipCompletely()
+
+		then: "order can be shipped"
+		readyToShip
+		2 * inventory.availableOfProduct( _ , _) >> true
+		0 * _
+	}
+	
+	
+	
+
+	
+	
+}
+
