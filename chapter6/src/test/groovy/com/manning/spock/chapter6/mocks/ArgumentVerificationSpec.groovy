aac31d20d69c8a5b7ba204c3d3b198c46dd73282
Kostis Kapelonis <kkapelon@gmail.com>
2015-05-22 22:40:44 +0300

    Added example with argument verification

---
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/ArgumentVerificationSpec.groovy
@@ -0,0 +1,75 @@
+package com.manning.spock.chapter6.mocks
+
+import spock.lang.*
+
+import com.manning.spock.chapter6.Basket
+import com.manning.spock.chapter6.Product
+import com.manning.spock.chapter6.stubs.WarehouseInventory
+
+@Subject(BillableBasket.class)
+class ArgumentVerificationSpec extends spock.lang.Specification{
+
+	def "vip status is correctly passed to credit card - simple"() {
+		given: "an basket, a customer and a TV"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		BillableBasket basket = new BillableBasket()
+		Customer customer = new Customer(name:"John",vip:false,creditCard:"testCard")
+
+		and:"a credit card service"
+		CreditCardProcessor creditCardSevice = Mock(CreditCardProcessor)
+		basket.setCreditCardProcessor(creditCardSevice)
+
+		when: "user checks out the tv"
+		basket.addProduct tv
+		basket.addProduct camera
+		basket.checkout(customer)
+
+		then: "credit card is charged"
+		1 * creditCardSevice.sale(1550, customer)
+	}
+	
+	def "vip status is correctly passed to credit card - vip"() {
+		given: "an basket, a customer and a TV"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		BillableBasket basket = new BillableBasket()
+		Customer customer = new Customer(name:"John",vip:false,creditCard:"testCard")
+
+		and:"a credit card service"
+		CreditCardProcessor creditCardSevice = Mock(CreditCardProcessor)
+		basket.setCreditCardProcessor(creditCardSevice)
+
+		when: "user checks out the tv"
+		basket.addProduct tv
+		basket.addProduct camera
+		basket.checkout(customer)
+
+		then: "credit card is charged"
+		1 * creditCardSevice.sale(1550, { client -> client.vip == false})
+	}
+	
+	def "vip status is correctly passed to credit card - full"() {
+		given: "an basket, a customer and a TV"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		Product camera = new Product(name:"panasonic",price:350,weight:2)
+		BillableBasket basket = new BillableBasket()
+		Customer customer = new Customer(name:"John",vip:false,creditCard:"testCard")
+
+		and:"a credit card service"
+		CreditCardProcessor creditCardSevice = Mock(CreditCardProcessor)
+		basket.setCreditCardProcessor(creditCardSevice)
+
+		when: "user checks out the tv"
+		basket.addProduct tv
+		basket.addProduct camera
+		basket.checkout(customer)
+
+		then: "credit card is charged"
+		1 * creditCardSevice.sale({amount -> amount == basket.findOrderPrice()}, { client -> client.vip == false})
+	}
+
+
+	
+}
+
