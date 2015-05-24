f9bf8b217382d907969f6ffe73634379bcd2f394
Kostis Kapelonis <kkapelon@gmail.com>
2015-05-24 23:27:14 +0300

    Minor fixes in block descriptions.

--- chapter6/src/test/groovy/com/manning/spock/chapter6/stubs/SimpleStubbingSpec.groovy
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/stubs/SimpleStubbingSpec.groovy
@@ -43,22 +43,22 @@ class SimpleStubbingSpec extends spock.lang.Specification{
 	}
 	
 	def "If warehouse has not all products, order cannot be shipped"() {
-		given: "an basket and a TV"
+		given: "an basket, a TV and a camera"
 		Product tv = new Product(name:"bravia",price:1200,weight:18)
 		Product camera = new Product(name:"panasonic",price:350,weight:2)
 		Basket basket = new Basket()
 		
-		and:"an empty warehouse"
+		and:"a warehouse with partial availability"
 		WarehouseInventory inventory = Stub(WarehouseInventory)
 		inventory.availableOfProduct("bravia",1) >> true
 		inventory.availableOfProduct("panasonic",1) >> false
 		basket.setWarehouseInventory(inventory)
 
-		when: "user checks out the tv"
+		when: "user checks out both products"
 		basket.addProduct tv
 		basket.addProduct camera
 
-		then: "order can be shipped right away"
+		then: "order cannoot be shipped right away"
 		!basket.canShipCompletely()
 	}
 	
