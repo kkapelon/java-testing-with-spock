d93dea41992c94bcffc0d5dcb2b36f7b58e5d60a
Kostis Kapelonis <kkapelon@gmail.com>
2015-05-30 20:14:16 +0300

    Better block descriptions

--- chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/OrderMockingSpec.groovy
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/OrderMockingSpec.groovy
@@ -9,6 +9,27 @@ import com.manning.spock.chapter6.stubs.WarehouseInventory
 @Subject(BillableBasket.class)
 class OrderMockingSpec extends spock.lang.Specification{
 
+	def "credit card connection is closed down in the end - incorrect"() {
+		given: "a basket, a customer and a TV"
+		Product tv = new Product(name:"bravia",price:1200,weight:18)
+		BillableBasket basket = new BillableBasket()
+		Customer customer = new Customer(name:"John",vip:false,creditCard:"testCard")
+
+		and: "a credit card service"
+		CreditCardProcessor creditCardSevice = Mock(CreditCardProcessor)
+		basket.setCreditCardProcessor(creditCardSevice)
+
+		when: "user checks out the tv"
+		basket.addProduct tv
+		basket.checkout(customer)
+
+		//Order of invocations in then block does not matter to Spock
+		then: "credit card is charged and CC service is closed down"
+		1 * creditCardSevice.shutdown()
+		1 * creditCardSevice.sale(1200,customer)
+	}
+
+
 	def "credit card connection is closed down in the end"() {
 		given: "a basket, a customer and a TV"
 		Product tv = new Product(name:"bravia",price:1200,weight:18)
@@ -23,30 +44,10 @@ class OrderMockingSpec extends spock.lang.Specification{
 		basket.addProduct tv
 		basket.checkout(customer)
 
-		then: "credit card is charged"
-		1 * creditCardSevice.shutdown()
-		1 * creditCardSevice.sale(1200,customer)
-	}
-
-
-	def "credit card connection is closed down in the end (alt)"() {
-		given: "a basket, a customer and a TV"
-		Product tv = new Product(name:"bravia",price:1200,weight:18)
-		BillableBasket basket = new BillableBasket()
-		Customer customer = new Customer(name:"John",vip:false,creditCard:"testCard")
-
-		and: "a credit card service"
-		CreditCardProcessor creditCardSevice = Mock(CreditCardProcessor)
-		basket.setCreditCardProcessor(creditCardSevice)
-
-		when: "user checks out the tv"
-		basket.addProduct tv
-		basket.checkout(customer)
-
-		then: "credit card is charged"
+		then: "credit card is charged and"
 		1 * creditCardSevice.sale( _, _)
 
-		then: "credit card is charged"
+		then: "the credit card service is closed down"
 		1 * creditCardSevice.shutdown()
 	}
 }
