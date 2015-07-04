a2400147e12f4d7df4556cf6c15fcfbef3836066
Kostis Kapelonis <kkapelon@gmail.com>
2015-07-05 01:53:34 +0300

    Added examples with Groovy SQL

--- chapter7/spring-standalone-swing/src/test/groovy/com/manning/spock/warehouse/product/DummyDatabaseGroovySqlSharedSpec.groovy
+++ chapter7/spring-standalone-swing/src/test/groovy/com/manning/spock/warehouse/product/DummyDatabaseGroovySqlSharedSpec.groovy
@@ -1,26 +1,46 @@
 package com.manning.spock.warehouse.product
 
+import groovy.sql.Sql
+
 import javax.sql.DataSource
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.*
-import org.springframework.transaction.annotation.Transactional
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction
+import org.springframework.transaction.annotation.Transactional;
 
-import groovy.sql.Sql
 import spock.lang.*
 
 @ContextConfiguration(locations = "classpath:reduced-test-context.xml")
-class DummyDatabaseGroovySqlWriteSpec extends spock.lang.Specification{
+@Transactional
+class DummyDatabaseGroovySqlSharedSpec extends spock.lang.Specification{
 	@Autowired
 	DataSource dataSource
 	
 	@Autowired
 	ProductLoader productLoader
+	
+	@Shared
+	Sql sql
+	
+	@BeforeTransaction
+	def emptyDatabase()
+	{
+		println "hello"
+		sql  = new Sql(dataSource)
+		sql.execute("DELETE FROM PRODUCT")
+	}
+	
+	@AfterTransaction
+	def deleteAll()
+	{
+		sql.execute("DELETE FROM PRODUCT")
+	}
 
 	def "Testing ordering of products"() {
 		given: "the creation of 3 new products"
-		Sql sql  = new Sql(dataSource)
+		
 		sql.execute("DELETE FROM PRODUCT")
 		sql.execute("INSERT INTO PRODUCT (id,name,price, weight,stock) VALUES (1, 'samsung',400,1,45);")
 		sql.execute("INSERT INTO PRODUCT (id,name,price, weight,stock) VALUES (2, 'bravia',1200,3,2);")
@@ -34,16 +54,10 @@ class DummyDatabaseGroovySqlWriteSpec extends spock.lang.Specification{
 		allProducts[0].name =="bravia"
 		allProducts[1].name =="canon"
 		allProducts[2].name =="samsung"
-		
-		cleanup: "remove inserted data"
-		sql.execute("DELETE FROM PRODUCT")
-		sql.close()
-		
 	}
 	
 	def "Testing ordering of products - improved"() {
 		given: "the creation of 3 new products"
-		Sql sql  = new Sql(dataSource)
 		sql.execute("DELETE FROM PRODUCT")
 		String insertProduct = "INSERT INTO PRODUCT (id,name,price, weight,stock) VALUES (?, ?,?,?,?);"
 		sql.execute(insertProduct,[1, 'samsung',400,1,45])
@@ -58,10 +72,6 @@ class DummyDatabaseGroovySqlWriteSpec extends spock.lang.Specification{
 		allProducts[0].name =="bravia"
 		allProducts[1].name =="canon"
 		allProducts[2].name =="samsung"
-		
-		cleanup: "remove inserted data"
-		sql.execute("DELETE FROM PRODUCT")
-		sql.close()
 	}
 }
 
