76dade383511dd963a8eba27be201554b5ffbfa1
Kostis Kapelonis <kkapelon@gmail.com>
2015-07-05 19:15:31 +0300

    Finished Spock tests with Spring rest client.

--- chapter7/rest-service-example/src/test/groovy/com/manning/spock/SpringRestSpec.groovy
+++ chapter7/rest-service-example/src/test/groovy/com/manning/spock/SpringRestSpec.groovy
@@ -1,9 +1,11 @@
+package com.manning.spock;
 
-package com.codepipes.wizard;
-
+import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
 
 import spock.lang.*
+
+import com.manning.spock.Product
 
 /**
  * Rest tests using Spring Rest template
@@ -21,7 +23,41 @@ class SpringRestSpec extends Specification {
 		
 		then: "the correct message is expected"
 		status == "Up and Running"
+	}
+	
+	def "Cleaning all products"() {
+		given: "a rest call is performed that deletes everything"
+		RestTemplate restTemplate = new RestTemplate()
+		restTemplate.delete("http://localhost:8080/rest-service-example/products")
 		
+		when: "a product list is requested"
+		List<Product> products = restTemplate.getForObject("http://localhost:8080/rest-service-example/products", List.class)
+		
+		then: "it should be empty"
+		products.size() == 0
+	}
+	
+	def "Creating a product"() {
+		given: "a rest template"
+		RestTemplate restTemplate = new RestTemplate()
+		
+		when: "a new product is created"
+		Product product = restTemplate.postForObject("http://localhost:8080/rest-service-example/products","unused",Product.class)
+		
+		and: "product list is requested again"
+		List<Product> products = restTemplate.getForObject("http://localhost:8080/rest-service-example/products", List.class)
+		
+		then: "it should have default values"
+		with(product)
+		{
+			name == "A product"
+			stock == 0
+			price == 0
+			weight == 0
+		}
+		
+		and: "product list should contain it"
+		products.size() == 1
 	}
 
 	
