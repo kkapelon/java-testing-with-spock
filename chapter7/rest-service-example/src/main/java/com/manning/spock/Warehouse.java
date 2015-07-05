9a2df2e01e61edd7bd215d2759c666038dddc2e8
Kostis Kapelonis <kkapelon@gmail.com>
2015-07-05 18:25:10 +0300

    Finished simple REST service

--- chapter7/rest-service-example/src/main/java/com/manning/spock/Warehouse.java
+++ chapter7/rest-service-example/src/main/java/com/manning/spock/Warehouse.java
@@ -6,9 +6,14 @@ package com.manning.spock;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 
 /**
  * @author kkapelon
@@ -41,6 +46,35 @@ public class Warehouse {
 
 		return product;
 
+	}
+	
+	@RequestMapping(value = "/products/{productId}", method = RequestMethod.GET)
+	public Product listProduct(@PathVariable Integer productId) {
+		if(productId<0|| productId >= existingProducts.size())
+		{
+			throw new ProductNotFoundException("product id not found"); 
+		}
+		return existingProducts.get(productId);
+	}
+	
+	@RequestMapping(value = "/products/{productId}/name", method = RequestMethod.PUT,params = "name")
+	public Product renameProduct(@PathVariable Integer productId,@RequestParam("name") String name) {
+		if(productId<0|| productId >= existingProducts.size())
+		{
+			throw new ProductNotFoundException("product id not found"); 
+		}
+		Product product = existingProducts.get(productId);
+		product.setName(name);
+		return product;
+	}
+	
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	private static  class ProductNotFoundException extends RuntimeException
+	{       
+	    private static final long serialVersionUID = 1L;    
+	    public ProductNotFoundException(String message) {
+	        super(message);
+	    }    
 	}
 
 }
