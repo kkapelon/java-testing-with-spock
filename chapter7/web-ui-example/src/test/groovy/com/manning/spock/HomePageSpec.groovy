8930366c00122e53c2b6f68ca7c30ffb2bd8e670
Kostis Kapelonis <kkapelon@gmail.com>
2015-07-07 00:13:42 +0300

    Added trivial geb spec.

---
+++ chapter7/web-ui-example/src/test/groovy/com/manning/spock/HomePageSpec.groovy
@@ -0,0 +1,47 @@
+
+package com.codepipes.wizard;
+
+import geb.spock.GebSpec
+import spock.lang.*
+import geb.spock.GebReportingSpec
+import geb.Browser
+
+/**
+ * Trivial Geb test
+ * @author Kostis
+ *
+ */
+class HomePageSpec extends GebSpec {
+
+	def "Trivial Geb test for homepage"() {
+		when: "I go to homepage"
+		Browser.drive {
+			go "http://localhost:8080/web-ui-example/index.html"
+		}
+		
+		then: "First page should load"
+		Browser.drive {
+			// make sure we actually got to the page
+			title == "Spock/Geb Web example"
+		}
+		
+	}
+	
+	def "Trivial Geb test for homepage -header check"() {
+		when: "I go to homepage"
+		Browser.drive {
+			go "http://localhost:8080/web-ui-example/index.html"
+		}
+		
+		then: "First page should load"
+		Browser.drive {
+			// make sure we actually got to the page
+			title == "Spock/Geb Web example"
+			$("h1").text() == "Java Testing with Spock - Sample code"
+			$(".active").text() == "Welcome"
+		}
+		
+	}
+
+	
+}
---
+++ chapter7/web-ui-example/src/test/resources/GebConfig.groovy
@@ -0,0 +1,11 @@
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+import org.openqa.selenium.firefox.FirefoxDriver
+
+driver = { new FirefoxDriver() }
+
+baseUrl = "http://gebish.org"
