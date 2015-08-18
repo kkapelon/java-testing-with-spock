30149eeb83e9978eee4def4ea5540db8dd4173ba
Kostis Kapelonis <kkapelon@gmail.com>
2015-08-18 20:34:15 +0300

    Removing code that was not used in the book.
    The corrent example is listing 6.18 and is already included in
    CardinalityMockingSpec.groovy

--- chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/ArgumentTypeVerificationSpec.groovy
+++ chapter6/src/test/groovy/com/manning/spock/chapter6/mocks/ArgumentTypeVerificationSpec.groovy
@@ -50,28 +50,6 @@ class ArgumentTypeVerificationSpec extends spock.lang.Specification{
 		2 * inventory.isProductAvailable(_ as String ,_ as Integer) >> true
 	}
 	
-	def "Warehouse is queried for each product - stric "() {
-		given: "a basket, a TV and a camera"
-		Product tv = new Product(name:"bravia",price:1200,weight:18)
-		Product camera = new Product(name:"panasonic",price:350,weight:2)
-		Basket basket = new Basket()
-		
-		and: "a warehouse with limitless stock"
-		WarehouseInventory inventory = Mock(WarehouseInventory)
-		basket.setWarehouseInventory(inventory)
-
-		when: "user checks out both products"
-		basket.addProduct tv
-		basket.addProduct camera
-		boolean readyToShip = basket.canShipCompletely()
-
-		then: "order can be shipped"
-		readyToShip
-		2 * inventory.isProductAvailable(_, _) >> true
-		1 * inventory.isEmpty() >> false
-		0 * inventory._
-	}
-	
 	
 }
 
